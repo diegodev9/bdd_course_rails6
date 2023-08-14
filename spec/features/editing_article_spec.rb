c146a7ce4050ea54b0b8c4f8117a6a2ed85deb3c
diegodev9 <diegodev9@gmail.com>
2023-08-14 09:47:45 -0300

    setup users sign in

--- spec/features/editing_article_spec.rb
+++ spec/features/editing_article_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 require 'support/database_cleaner'
+require 'support/devise'
 
 RSpec.describe 'Editing an article', type: :feature do
   before do
     @article = Article.create(title: 'Title One', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur fugit impedit inventore, laudantium neque nihil porro ut! Aliquam blanditiis, commodi earum est facere ipsam magni maxime, nihil quod rem sint?')
+    @user = User.create(email: 'user@example.com', password: 'password')
+    sign_in @user
   end
 
   scenario 'A user updates an article' do
