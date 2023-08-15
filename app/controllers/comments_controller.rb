f6ca70a07d5203988af9e99c65f4c72eef61375b
diegodev9 <diegodev9@gmail.com>
2023-08-15 09:14:58 -0300

    force sign in before posting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,17 @@ class CommentsController < ApplicationController
   before_action :set_article
 
   def create
-    @comment = @article.comments.build(comment_params)
-    @comment.user = current_user
-
-    if @comment.save
-      redirect_to article_path(@article), notice: 'Comment has been created'
+    unless current_user
+    redirect_to new_user_session_path, alert: 'Please sign in or sing up first'
     else
-      redirect_to article_path(@article), alert: 'Comment has not been created'
+      @comment = @article.comments.build(comment_params)
+      @comment.user = current_user
+
+      if @comment.save
+        redirect_to article_path(@article), notice: 'Comment has been created'
+      else
+        redirect_to article_path(@article), alert: 'Comment has not been created'
+      end
     end
   end
 
--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,7 +1,38 @@
 require 'rails_helper'
+require 'support/database_cleaner'
+require 'support/devise'
 
 RSpec.describe "Comments", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  before do
+    @user = User.create(email: 'user@example.com', password: 'password')
+    @user2 = User.create(email: 'user2@example.com', password: 'password')
+    @article = Article.create(title: 'articulo 1', body: 'body 1', user: @user)
+  end
+
+  describe "POST /articles/:id/comments" do
+    context 'with a non-signed in user' do
+      before { post "/articles/#{@article.id}/comments", params: { comment: { body: 'Awesome blog' } }, as: :json }
+
+      it 'redirect user to the sign in page' do
+        flash_message = 'Please sign in or sing up first'
+        expect(response).to redirect_to(new_user_session_path)
+        expect(response).to have_http_status(302)
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+
+    context 'with a signed in user' do
+      before do
+        sign_in @user2
+        post "/articles/#{@article.id}/comments", params: { comment: { body: 'Awesome blog' } }, as: :json
+      end
+
+      it 'create the comment successfully' do
+        flash_message = 'Comment has been created'
+        expect(response).to redirect_to(article_path(@article.id))
+        expect(response).to have_http_status(302)
+        expect(flash[:notice]).to eq flash_message
+      end
+    end
   end
 end
